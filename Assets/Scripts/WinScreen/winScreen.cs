f48ec7cdca0f066681cfa3c09622c83c2adb5e5e
Mike (64916) <m.connotte@sintlucasedu.nl>
2023-09-20 11:15:40 +0200

    Scenes and most scripts made

--- Assets/Scripts/WinScreen/winScreen.cs
+++ Assets/Scripts/WinScreen/winScreen.cs
@@ -3,21 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class mainMenu : MonoBehaviour
+public class winScreen : MonoBehaviour
 {
-    public void level1()
+    public void mainMenu()
     {
-        SceneManager.LoadScene(1);
-    }
-
-    public void level2()
-    {
-        SceneManager.LoadScene(2);
-    }
-
-    public void level3()
-    {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(0);
     }
 
     public void gameQuit()
