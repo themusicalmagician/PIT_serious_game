24e071e235eec58d37d81bc959beb04465dfea4f
Mike (64916) <m.connotte@sintlucasedu.nl>
2023-10-31 09:17:40 +0100

    Antwoord lijsten

---
+++ Assets/Scripts/Level 3/Answers.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.Sqlite;
+using UnityEngine;
+
+public class Answers : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> answers = new List<Sprite>();
+    [SerializeField] private List<bool> isCorrect = new List<bool>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
