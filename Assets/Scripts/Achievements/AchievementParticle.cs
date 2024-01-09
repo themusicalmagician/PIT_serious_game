1012d53c7273a66fbaa182df20e9563a8f21d945
Mike (64916) <m.connotte@sintlucasedu.nl>
2024-01-09 12:44:12 +0100

    Particle system works

--- Assets/Scripts/Achievements/AchievementParticle.cs
+++ Assets/Scripts/Achievements/AchievementParticle.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AchievementParticle : MonoBehaviour
@@ -7,9 +8,13 @@ public class AchievementParticle : MonoBehaviour
     [SerializeField] private ParticleSystem Left;
     [SerializeField] private ParticleSystem Right;
 
-    public void Start()
+    public void Awake()
     {
+        Left = GameObject.FindWithTag("ParticleL").GetComponent<ParticleSystem>();
+        Right = GameObject.FindWithTag("ParticleR").GetComponent<ParticleSystem>();
+
         Left.Play();
+        
         Right.Play();
     }
 }
